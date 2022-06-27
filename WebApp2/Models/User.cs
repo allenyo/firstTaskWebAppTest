ced05fc4d813dec4f84bbd0ce542c4665ed69896
cdigital <cdigital@CDB33>
2022-06-27 17:00:49 +0400

    Add project files.

---
+++ WebApp2/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApp2.Models
+{
+    internal class User
+    {
+
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public int BirthYear { get; set; }
+        public int BirthMonth { get; set; }
+        public int BirthDay { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ WebApp2/Models/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp2.Models
+{
+    internal class UserContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; } = null!;
+
+       
+
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+            Database.EnsureCreated();
+
+        }
+        
+
+    }
+}

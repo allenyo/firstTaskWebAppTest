93001763c2a4d5663d44120427e5cbb4c98b2f1b
Alen Sahakyan <alen.sahakyan@cdigital.am>
2022-07-11 09:51:52 +0400

    version1.0.8

--- Domain/Validation/UserValidator.cs
+++ Domain/Validation/UserValidator.cs
@@ -13,9 +13,9 @@ namespace Domain.Validation
             RuleFor(x => x.LastName).Must(p => p == string.Empty || (char.IsUpper(p.Trim()[0]) && !p.Any(p => char.IsDigit(p))));
             RuleFor(x => x.Email).Must(p => p == string.Empty || ( p.Length > 4) && Regex.IsMatch(p,"^[a-zA-Z0-9-_.@]*$"));
             RuleFor(x => x.Phone).Must(s => (s == string.Empty) ||  s.Length > 4 && (s.StartsWith("+") && s.Skip('+').All(char.IsDigit) ) || (s.All(char.IsDigit) && s.Length > 4));
-            RuleFor(x => x.BirthYear).Must(p=> int.TryParse(p, out int pp) && pp<2023 && pp>1920);
-            RuleFor(x => x.BirthMonth).Must(p => int.TryParse(p, out int pp) && pp<=12 && pp>0 );
-            RuleFor(x => x.BirthDay).Must(p => int.TryParse(p, out int pp) && pp <= 31 && pp > 0);
+            RuleFor(x => x.BirthYear).Must(p=> p== string.Empty || int.TryParse(p, out int pp) && pp<2023 && pp>1920);
+            RuleFor(x => x.BirthMonth).Must(p => p== string.Empty || int.TryParse(p, out int pp) && pp<=12 && pp>0 );
+            RuleFor(x => x.BirthDay).Must(p => p== string.Empty || int.TryParse(p, out int pp) && pp <= 31 && pp > 0);
             RuleFor(x => x.Time).LessThanOrEqualTo(DateTime.Now);
 
         }
